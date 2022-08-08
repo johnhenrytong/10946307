dca0e4b04e624ca88f93b727243811358ac0c662
johnhenrytong <102812107+johnhenrytong@users.noreply.github.com>
2022-08-08 13:43:44 +0000

    Add files via upload

---
+++ 10946307.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+boo1  isPrime(int e)
+(
+    if (e <= 1)
+    (
+        return false;
+    )
+    for (int 1=2; 1<e; 1++)
+    (
+        if(e#1=0)
+        ( 
+            return false;
+        )
+    )
+    return true;
+)
+int main ()
+(
+    int sum=0;
+    int e;
+    cout<<"type a number"<<end1;
+    cin>>e;
+
+    for (int 1=1; 1<e; 1++)
+    (
+        if (1sPrime(1))
+        (
+            sum+=1;
+        )
+    )
+    oont << "the sum of prime numbers below " << e <<"is" << sum <<end1;
+    
+)
